aea8440b6c37a45401a6f1f2d97fb6b6839688f9
nabil-elsadany <nabilelsadany14@gmail.com>
2024-11-09 15:00:41 +0200

    first commit

--- Program.cs
+++ Program.cs
@@ -6,33 +6,25 @@ namespace earasoft_task1
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Welcome to the laundry");
-            //Console.Write("Enter the number of small rugs:");
-            //int small_rugs=int.Parse(Console.ReadLine());
-            //Console.Write("Enter the number of large rugs:");
-            //int large_rugs = int.Parse(Console.ReadLine());
-            //Console.WriteLine("The price of the small rug is $25.");
-            //Console.WriteLine("The price of the large rug is $35.");
-            //int total_small_rugs=small_rugs*25;
-            //int total_large_rugs = large_rugs * 35;
-            //Console.WriteLine($"Total price of small carpet :{total_small_rugs}");
-            //Console.WriteLine($"Total price of large carpet :{total_large_rugs}");
-            //int total = total_small_rugs + total_large_rugs;
-            //Console.WriteLine($"Total price : {total}");
-            //Console.WriteLine("the tax is 6% .");
-            //double tax = total * 0.06;
-            //Console.WriteLine($"Total tax : {tax}");
-            //double Total_price_with_tax = tax + total;
-            //Console.WriteLine($"Total price with tax : {Total_price_with_tax}");
-            //Console.WriteLine("This estimate is valid for 30 days.");
-
-
-            double X = 10.5;
-            int Y = 20;
-            double Z = X + Y;
-
-            Console.WriteLine($"Equation: {X} + {Y} = {Z:p}");
-            Console.ReadKey();
+            Console.WriteLine("Welcome to the laundry");
+            Console.Write("Enter the number of small rugs:");
+            int small_rugs=int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of large rugs:");
+            int large_rugs = int.Parse(Console.ReadLine());
+            Console.WriteLine("The price of the small rug is $25.");
+            Console.WriteLine("The price of the large rug is $35.");
+            int total_small_rugs=small_rugs*25;
+            int total_large_rugs = large_rugs * 35;
+            Console.WriteLine($"Total price of small carpet :{total_small_rugs}");
+            Console.WriteLine($"Total price of large carpet :{total_large_rugs}");
+            int total = total_small_rugs + total_large_rugs;
+            Console.WriteLine($"Total price : {total}");
+            Console.WriteLine("the tax is 6% .");
+            double tax = total * 0.06;
+            Console.WriteLine($"Total tax : {tax}");
+            double Total_price_with_tax = tax + total;
+            Console.WriteLine($"Total price with tax : {Total_price_with_tax}");
+            Console.WriteLine("This estimate is valid for 30 days");
 
 
 
